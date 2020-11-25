0ef552fbe6c56a78c7d33522dd3f8834760f22a9
Nageswari SV <65342122+Nageswari-droid@users.noreply.github.com>
2020-11-25 20:30:54 +0530

    Left Right numbers

---
+++ CharacterOcc/src/com/CharacterClass.java
@@ -0,0 +1,22 @@
+package com;
+
+public class CharacterClass {
+	public static void main(String args[]) {
+		int num = 101;
+		int count = 0;
+		
+		for(int i = 1 ; i <= num ; i++) {
+			
+			int temp = i;
+			int rem;
+			
+			while(temp != 0) {
+				rem = temp % 10;
+				count++;
+				temp = temp / 10;
+			}
+		}
+		
+		System.out.print(count);
+	}
+}
---
+++ LeftRight/src/com/LeftRightClass.java
@@ -0,0 +1,43 @@
+package com;
+
+public class LeftRightClass {
+	public static void main(String args[]) {
+		int num = 6;
+		int[] numArr = { 1, 2, 3, 4, 5, 6 };
+		int flag = 0;
+		int count = 0;
+
+		for (int i = 1; i < numArr.length - 1; i++) {
+			int first = 0;
+			int last = i + 1;
+			count = 0;
+			
+			while (first != i) {
+				if (numArr[first] < numArr[i]) {
+					count++;
+					first++;
+				} else {
+					break;
+				}
+			}
+
+			while (last != numArr.length) {
+				if (numArr[last] > numArr[i]) {
+					count++;
+					last++;
+				} else {
+					break;
+				}
+			}
+
+			if (count == num - 1) {
+				flag = 1;
+				System.out.println(numArr[i]);
+			}
+		}
+		
+		if(flag == 0) {
+			System.out.print(-1);
+		}
+	}
+}
