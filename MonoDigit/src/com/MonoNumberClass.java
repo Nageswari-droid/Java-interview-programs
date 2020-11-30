e6703e7598f32b410dca26ffbf3a40029a419a80
Nageswari SV <65342122+Nageswari-droid@users.noreply.github.com>
2020-11-30 15:50:13 +0530

    Array with same elements sum problem

--- MonoDigit/src/com/MonoNumberClass.java
+++ MonoDigit/src/com/MonoNumberClass.java
@@ -4,17 +4,23 @@ import java.util.*;
 
 public class MonoNumberClass {
 	public static void main(String args[]) {
-		int num = 772581;
-		String numStr = String.valueOf(num);
+		int num = 72581;
+		String[] numStr = String.valueOf(num).split("");
 		String result = "";
 
 		int iPointer = 0;
 		int jPointer = 1;
 
-		List<String> resultList = new ArrayList<>();
+		List<Integer> resultList = new ArrayList<>();
 
-		for (int i = 0; i < numStr.length(); i++) {
-			
+		for (int i = 0; i < numStr.length; i++) {
+			for (int j = 0; j < numStr.length; j++) {
+				if (jPointer < numStr.length - 1) {
+					resultList.add(Integer.parseInt(numStr[iPointer]) + Integer.parseInt(numStr[jPointer]));
+					iPointer++;
+					jPointer++;
+				}
+			}
 		}
 	}
 }
