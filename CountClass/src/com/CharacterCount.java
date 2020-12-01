148f6863aac30f9b9e4f63ed2a6fc43b76c3af52
Nageswari SV <65342122+Nageswari-droid@users.noreply.github.com>
2020-12-02 00:36:12 +0530

    Change in diamond problem

--- CountClass/src/com/CharacterCount.java
+++ CountClass/src/com/CharacterCount.java
@@ -4,7 +4,7 @@ public class CharacterCount {
 
 	public static void main(String args[]) {
 
-		String input = "affffffffffbbbbbbbbbbbbbbbbbbbbccccccccccccccce";
+		String input = "aaaabbccccffe";
 		String result = "";
 
 		int count = 1;
--- NumberDiamond/src/com/NumberDiamondClass.java
+++ NumberDiamond/src/com/NumberDiamondClass.java
@@ -2,48 +2,63 @@ package com;
 
 public class NumberDiamondClass {
 	public static void main(String args[]) {
-		int num = 5;
-		
+		int num = 9;
+
 		int iPointer = (num / 2) + 1;
-		
+		int midPoint = iPointer;
+
 		int start = 1;
 		int k = iPointer;
-		
+
+		int j = 0;
+
 		String[] str = new String[num + 2];
-		
-		for(int i = 0 ; i < num + 2; i++) {
-			str[i] = " "; 
+
+		for (int l = 0; l < num + 2; l++) {
+			str[l] = " ";
 		}
-		
-		for(int i = 0 ; i < num - 1; i++) {
+
+		for (int i = 0; i < num - 1; i++) {
 			k = iPointer;
-			for(int j = 0 ; j <= i ; j++) {
-				str[iPointer] = String.valueOf(start);
-				start++;
-				iPointer = iPointer - 2;
+			for (j = 0; j <= i; j++) {
+				if (iPointer < (num + 2)) {
+					str[iPointer] = String.valueOf(start);
+					start++;
+					iPointer = iPointer - 2;
+				} else {
+					break;
+				}
 			}
+
 			iPointer = k + 1;
-			for(int m = 0 ; m < str.length ; m++) {
-				System.out.print(str[m] + " ");
-				str[m] = " "; 
+
+			if (iPointer <= (num + 2)) {
+				for (int m = 0; m < str.length; m++) {
+					System.out.print(str[m] + " ");
+					str[m] = " ";
+				}
+				System.out.println("");
 			}
-			System.out.println(" ");
 		}
-		
+
 		iPointer = 0;
 		start = start - 1;
-		
-		for(int i = num - 2 ; i >= 0; i--) {
+
+		for (int i = num - (midPoint - 1); i >= 0; i--) {
 			k = iPointer;
-			for(int j = 0 ; j <= i ; j++) {
-				str[iPointer] = String.valueOf(start);
-				start--;
-				iPointer = iPointer + 2;
+			for (j = 0; j <= i; j++) {
+				if ((iPointer < num + 2) && (start >= 1)) {
+					str[iPointer] = String.valueOf(start);
+					start--;
+					iPointer = iPointer + 2;
+				} else {
+					break;
+				}
 			}
 			iPointer = k + 1;
-			for(int m = 0 ; m < str.length ; m++) {
+			for (int m = 0; m < str.length; m++) {
 				System.out.print(str[m] + " ");
-				str[m] = " "; 
+				str[m] = " ";
 			}
 			System.out.println(" ");
 		}
