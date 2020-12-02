de582624b583ec8b797391ed71a69ad1b45b00d4
Nageswari SV <65342122+Nageswari-droid@users.noreply.github.com>
2020-12-03 01:22:30 +0530

    Sub string palindrome

---
+++ javaNew/SubString/src/com/SubStringClass.java
@@ -0,0 +1,64 @@
+package com;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class SubStringClass {
+	public static void main(String args[]) {
+
+		String str = "abccbaef";
+		String reverseStr = "";
+
+		LinkedHashMap<String, String> palindrome = new LinkedHashMap<>();
+
+		for (int i = 0; i < str.length() + 1; i++) {
+			reverseStr = "";
+			for (int j = i; j < str.length() + 1; j++) {
+				if (reverseStr.length() >= 2) {
+
+					boolean result = reverseMethod(reverseStr);
+
+					if (result == true) {
+						String index = "Index from " + String.valueOf(i) + " to " + String.valueOf(j - 1); 
+						palindrome.put(reverseStr, index);
+					}
+					if(j < str.length()) {
+						reverseStr += str.charAt(j);
+					}
+
+				} else {
+					if(j < str.length()) {
+						reverseStr = reverseStr + str.charAt(j);
+					}
+				}
+			}
+		}
+
+		for(Entry<String, String> map : palindrome.entrySet()) {
+			System.out.println(map.getKey() + " " + map.getValue());
+		}
+	}
+
+	public static boolean reverseMethod(String reverse) {
+
+		int i = 0;
+		int j = reverse.length() - 1;
+
+		int count = 0;
+
+		while (i < reverse.length() && j < reverse.length()) {
+			if (reverse.charAt(i) == reverse.charAt(j)) {
+				i++;
+				j--;
+				count++;
+			} else {
+				break;
+			}
+		}
+		if (count == (reverse.length())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
