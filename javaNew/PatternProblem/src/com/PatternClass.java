bd5e478b4999d9cc35c105b495c80db447540b6b
Nageswari SV <65342122+Nageswari-droid@users.noreply.github.com>
2020-12-04 20:28:20 +0530

    Pattern problems

---
+++ javaNew/PatternProblem/src/com/PatternClass.java
@@ -0,0 +1,34 @@
+package com;
+
+public class PatternClass {
+	public static void main(String args[]) {
+		
+		int input = 3;
+		int num = 1;
+		int[][] matArr = new int[input][input];
+		
+		for(int j = 0 ; j < input ; j++) {
+			if(j % 2 == 0) {
+				int temp = input - 1;
+				for(int i = temp ; i >= 0 ; i--) {
+					matArr[i][j] = num;
+					num++;
+				}
+			}
+			else {
+				int temp = 0;
+				for(int i = temp ; i < input ; i++) {
+					matArr[i][j] = num;
+					num++;
+				}
+			}
+		}
+		
+		for(int i = 0 ; i < input ; i++) {
+			for(int j = 0 ; j < input ; j++) {
+				System.out.print(matArr[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+}
