f135dd9799c0a71c01604629662199e534ffd0ea
Nageswari SV <65342122+Nageswari-droid@users.noreply.github.com>
2021-04-30 21:57:16 +0530

    Deleted inheritence

--- Design Pattern Using OOP/2.Inheritance/src/com/ChildClass.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-public class ChildClass extends ParentClass {
-	public void printAgain() {
-		System.out.println("Hello World!");
-	}
-}
--- Design Pattern Using OOP/2.Inheritance/src/com/ParentClass.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-public class ParentClass {
-	public void print() {
-		System.out.println("Hello!");
-	}
-}
